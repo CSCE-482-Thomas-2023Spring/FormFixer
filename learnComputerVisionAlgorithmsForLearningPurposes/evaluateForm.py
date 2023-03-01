d99e3faf09b5d99ea9dd251c531c0762ac329ef6
cswordsaggie <68725761+cswordsaggie@users.noreply.github.com>
2023-03-01 09:40:22 -0600

    Add file dedicated to evaluating Form

---
+++ learnComputerVisionAlgorithmsForLearningPurposes/evaluateForm.py
@@ -0,0 +1,15 @@
+
+
+#def evaluateExerciseNameXYZ(p_landmarks, etc...):
+#    print("Evaluating XYZ")
+
+
+
+def evaluateDeadliftArmWidth():
+    print("Evaluating arm width")
+
+def evaluateDeadliftForm():
+
+    print("Evaluating form....")
+    evaluateDeadliftArmWidth()
+    return 0
